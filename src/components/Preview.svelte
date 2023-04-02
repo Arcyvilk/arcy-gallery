5e94754812f3f529c5d864ba217cf16a3b1c76df
Arcyvilk <blazemakerrr@gmail.com>
2023-04-02 20:23:02 +0200

    refactor: 💡 better handling of image preview

--- src/components/Preview.svelte
+++ src/components/Preview.svelte
@@ -34,22 +34,26 @@
     height: 100vh;
     background-color: #0b0b0bcc;
     box-sizing: border-box;
+    padding: 24px;
+    gap: 24px;
     &__img {
       box-shadow: 0 0 50px 20px #0b0b0b;
       border: 5px solid #000000;
+      max-height: 100%;
+      max-width: 100%;
+      object-fit: cover;
     }
     &__left {
       display: flex;
       align-items: center;
       justify-content: center;
-      flex: 1 1 50%;
+      flex: 0 0 70%;
       box-sizing: border-box;
     }
     &__right {
       display: flex;
       flex-direction: column;
-      flex: 1 1 50%;
-      padding-left: 24px;
+      flex: 0 0 30%;
       box-sizing: border-box;
       justify-content: center;
     }
--- src/images.ts
+++ src/images.ts
@@ -1,6 +1,15 @@
 import type { ImgTile } from './types';
 import { shuffleArray } from './utils/shuffleArray';
 
+// import img01 from './_data/img01.jpg';
+// import img02 from './_data/img02.jpg';
+// import img03 from './_data/img03.jpg';
+// import img04 from './_data/img04.jpg';
+// import img05 from './_data/img05.jpg';
+// import img06 from './_data/img06.jpg';
+// import img07 from './_data/img07.jpg';
+// import img08 from './_data/img08.jpg';
+
 const img01 = 'https://cataas.com/cat/cute';
 const img02 = 'https://cataas.com/cat/funny';
 const img03 = 'https://cataas.com/cat/tabby';

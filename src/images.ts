1c30830192fd8c8591ea42081700d351257357f5
Arcyvilk <blazemakerrr@gmail.com>
2023-04-02 17:06:20 +0200

    refactor: 💡 replace images with cats

--- src/images.ts
+++ src/images.ts
@@ -1,13 +1,14 @@
-import img01 from './_data/01.jpg';
-import img02 from './_data/02.jpg';
-import img03 from './_data/03.jpg';
-import img04 from './_data/04.jpg';
-import img05 from './_data/05.jpg';
-import img06 from './_data/06.jpg';
-import img07 from './_data/07.jpg';
-import img08 from './_data/08.jpg';
 import type { ImgTile } from './types';
 import { shuffleArray } from './utils/shuffleArray';
+
+const img01 = 'https://cataas.com/cat/cute';
+const img02 = 'https://cataas.com/cat/funny';
+const img03 = 'https://cataas.com/cat/tabby';
+const img04 = 'https://cataas.com/cat/bubble';
+const img05 = 'https://cataas.com/cat/sleep';
+const img06 = 'https://cataas.com/cat/smile';
+const img07 = 'https://cataas.com/cat/play';
+const img08 = 'https://cataas.com/cat/shoe';
 
 export const images: ImgTile[] = shuffleArray([
   { url: img03, thumbnail: img03, title: 'Title', description: 'Description' },
